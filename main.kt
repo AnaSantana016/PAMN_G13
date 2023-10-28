8fb3bda4c90d87df3d04c1254d42b5f18140c52a
Alejandro  David Arzola Saavedra <90756437+AlejandroDavidArzolaSaavedra@users.noreply.github.com>
2023-10-28 18:16:29 +0100

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,6 +1,6 @@
 ///////////////////////////////////////
 // PATRON DE DISEÑO FACTORY EN ANDROID
-// @DATE: 24/10/2023
+// @DATE: 28/10/2023
 // @AUTHORA: ADCSO
 // @UTHOR: ADAS
 /////////////////////////////////////
@@ -37,45 +37,31 @@ interface FabricaNotas {
 
 // Implementación concreta de la fábrica para notas de texto
 class FabricaNotaTexto(private val contenido: String) : FabricaNotas {
-    override fun crearNota(): Nota {
-        return NotaTexto(contenido)
-    }
+    override fun crearNota() = NotaTexto(contenido)
 }
 
 // Implementación concreta de la fábrica para notas de imagen
 class FabricaNotaImagen(private val rutaImagen: String) : FabricaNotas {
-    override fun crearNota(): Nota {
-        return NotaImagen(rutaImagen)
-    }
+    override fun crearNota() = NotaImagen(rutaImagen)
 }
 
 // Implementación concreta de la fábrica para notas de audio
 class FabricaNotaAudio(private val rutaAudio: String) : FabricaNotas {
-    override fun crearNota(): Nota {
-        return NotaAudio(rutaAudio)
-    }
+    override fun crearNota() = NotaAudio(rutaAudio)
 }
 
 // Implementación concreta de la fábrica para notas de video
 class FabricaNotaVideo(private val rutaVideo: String) : FabricaNotas {
-    override fun crearNota(): Nota {
-        return NotaVideo(rutaVideo)
-    }
+    override fun crearNota() = NotaVideo(rutaVideo)
 }
 
-fun main() {
-    // Crear fábricas de notas
-    val fabricaTexto: FabricaNotas = FabricaNotaTexto("Contenido de la nota de texto")
-    val fabricaImagen: FabricaNotas = FabricaNotaImagen("ruta/imagen.jpg")
-    val fabricaAudio: FabricaNotas = FabricaNotaAudio("ruta/audio.mp3")
-    val fabricaVideo: FabricaNotas = FabricaNotaVideo("ruta/video.mp4")
-	
+fun main() {	
     // Crear notas utilizando las fábricas
-    val notaTexto: Nota = fabricaTexto.crearNota()
-    val notaImagen: Nota = fabricaImagen.crearNota()
-    val notaAudio: Nota = fabricaAudio.crearNota()
-    val notaVideo: Nota = fabricaVideo.crearNota()
-    
+    val notaTexto = FabricaNotaTexto("Contenido de la nota de texto").crearNota()
+    val notaImagen = FabricaNotaImagen("ruta/imagen.jpg").crearNota()
+    val notaAudio = FabricaNotaAudio("ruta/audio.mp3").crearNota()
+    val notaVideo = FabricaNotaVideo("ruta/video.mp4").crearNota()
+	
     // Mostrar el contenido de las notas
     notaTexto.mostrarContenido()
     notaImagen.mostrarContenido()
